4f3a50561847548e9d8964c1ec2fabfb45f4004c
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 23:48:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli = []
+    puts "The line is currently empty."
+  elsif katz_deli != []
+    katz_deli.each_with_index do |person, index|
+      katz_deli << "The line currently: #{index + 1}. #{person}"
+    end
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  
+end
