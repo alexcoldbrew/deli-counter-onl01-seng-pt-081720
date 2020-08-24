1ce82ff8375717e7c2be1e3340aa6d84713858a6
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 23:54:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
-  elsif katz_deli !== []
+  elsif katz_deli != []
     katz_deli.each_with_index do |person, index|
       katz_deli << "The line currently: #{index + 1}. #{person}"
     end
